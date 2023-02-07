5e8f03557ca4da29050251e8b83f7ef1158e0cb2
Aung Kyaw Wai Htun <105596104+akwhtun@users.noreply.github.com>
2023-02-07 14:31:34 +0630

    login and register page

---
+++ js/counter.js
@@ -0,0 +1,33 @@
+var counter = document.getElementsByClassName("counter")[0];
+counter.innerHTML= 1;
+var a = 1;
+var final = 600;
+var isFinish = false;
+
+// let animation = setInterval( () => {
+//     counter.innerHTML = a;
+//     a++;
+//     if( a > final){
+//         isFinish = true;
+//         clearInterval(animation)
+//     }
+// }, 10);
+
+// let animation =() =>  setInterval ( () => {
+//     counter.innerHTML = a;
+//     a++;
+//     if( a > final){
+//         isFinish = true;
+//     }
+//     if(isFinish){
+//         stop();
+//     }
+// }, 10);
+
+// let stop = () => {
+//     clearInterval(animation)
+// }
+
+// animation();
+// // animation();
+
